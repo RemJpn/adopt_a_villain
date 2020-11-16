32503b185df9198283a0d4ddcfd856ce8498ce13
Remi Schwartz <schwartz.remi@gmail.com>
2020-11-16 18:07:00 +0100

    seed created

--- Gemfile
+++ Gemfile
@@ -62,3 +62,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Start"
+
+20.times do
+  new_user = User.new
+  new_user.first_name = Faker::Name.first_name
+  new_user.last_name = Faker::Name.last_name
+  new_user.email = Faker::Internet.email
+  new_user.password = Faker::Internet.password
+  new_user.is_villain = true
+
+  new_user.save!
+
+  new_villain = Villain.new
+  p new_villain.name = Faker::Superhero.unique.name
+  new_villain.description = Faker::GreekPhilosophers.quote
+  new_villain.nastiness = rand(1..5)
+  new_villain.daily_price = rand(20..3000)
+  new_villain.user = new_user
+
+  new_villain.save!
+
+end
+
+
+puts "Finish"
