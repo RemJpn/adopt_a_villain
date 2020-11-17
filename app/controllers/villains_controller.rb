251edef836ac0a6038ff7b8f9a63c587c319c2d0
baptiste derenne <derenne.baptiste@gmail.com>
2020-11-17 17:17:55 +0100

    Mo longer authentification in show villain page

--- app/controllers/villains_controller.rb
+++ app/controllers/villains_controller.rb
@@ -1,5 +1,5 @@
 class VillainsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @villains = Villain.all
