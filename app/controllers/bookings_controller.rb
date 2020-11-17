a302cab062a555531697cf01288309f5048c556a
AnneCecile Mechin <ac.mechin@gmail.com>
2020-11-17 18:05:12 +0100

    Booking index with basic infos

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,16 @@
 class BookingsController < ApplicationController
+   before_action :set_booking, only: [:show]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
