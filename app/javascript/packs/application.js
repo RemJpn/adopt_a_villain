54807c567cf6b2f6bb47ff7675e315ca94d4e40b
AnneCecile Mechin <ac.mechin@gmail.com>
2020-11-19 13:17:11 +0100

    date picker added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,13 +24,15 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { loadDynamicBannerText } from '../components/banner';
+import flatpickr from "flatpickr";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { loadDynamicBannerText } from '../components/banner';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  flatpickr("#datepicker", {});
   loadDynamicBannerText();
 });
